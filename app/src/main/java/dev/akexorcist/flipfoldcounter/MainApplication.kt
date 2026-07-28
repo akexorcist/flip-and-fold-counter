eb69cbb8f0714423095256b77bd26e96db4ebddb
Akexorcist <akexorcist@gmail.com>
2026-07-28 21:51:50 +0700

    test: verify real app change still triggers CI checks (throwaway, not to be merged)

--- app/src/main/java/dev/akexorcist/flipfoldcounter/MainApplication.kt
+++ app/src/main/java/dev/akexorcist/flipfoldcounter/MainApplication.kt
@@ -8,6 +8,7 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+
             androidContext(this@MainApplication)
             modules(appModule)
         }
